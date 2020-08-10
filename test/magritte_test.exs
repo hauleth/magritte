c251ee4d47160cf8e8b7e1df9b2074dd7c058d71
Łukasz Niemier <lukasz@niemier.pl>
2020-08-10 21:42:39 +0200

    test: add test for preserving correct order of arguments

--- test/magritte_test.exs
+++ test/magritte_test.exs
@@ -21,4 +21,13 @@ defmodule MagritteTest do
   test "works with calls without parens" do
     assert "1010" == (2 |> Integer.to_string(10, ...))
   end
+
+  test "multiple arguments are still passed as expected" do
+    assert {1, 2, 3} == (1 |> id(2, 3))
+    assert {1, 2, 3} == (1 |> id(..., 2, 3))
+    assert {1, 2, 3} == (2 |> id(1, ..., 3))
+    assert {1, 2, 3} == (3 |> id(1, 2, ...))
+  end
+
+  def id(a, b, c), do: {a, b, c}
 end
