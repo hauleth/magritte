db8c16c06280421c82c7a8f6bced6c5579bf9129
Łukasz Niemier <lukasz@niemier.pl>
2020-08-10 21:11:29 +0200

    docs: document package

--- mix.exs
+++ mix.exs
@@ -4,25 +4,34 @@ defmodule Magritte.MixProject do
   def project do
     [
       app: :magritte,
+      description: "Ceci n'est pas une pipe - extended pipe operator",
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
+      docs: [
+        main: "Magritte"
+      ],
+      package: package(),
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
+    []
+  end
+
+  defp package do
     [
-      extra_applications: [:logger]
+      licenses: ~w[MIT],
+      links: %{
+        "GitHub" => "https://github.com/hauleth/magritte"
+      }
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
 end
